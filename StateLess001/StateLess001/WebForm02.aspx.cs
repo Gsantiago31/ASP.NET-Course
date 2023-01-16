11df8ac2eaa87a193d4abc99a0195363c544cee7
Santiago <gomezgu31@gmail.com>
2023-01-15 22:22:33 -0600

    TESTASP and Stateless

---
+++ StateLess001/StateLess001/WebForm02.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateLess001
+{
+    public partial class WebForm02 : System.Web.UI.Page
+    {
+        private int conteo = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtConteo.Text = "0";
+            }
+        }
+
+        protected void btnIncrementa_Click(object sender, EventArgs e)
+        {
+            // Si ya se ha utilizado el viewstate con la variable click
+            if (ViewState["click"] != null)
+            {
+                // Leemo el valor, hacemos typecast, lo incrementamos y lo colocamos en la 
+                // variable para poder trabajaar con ese dato en nuestro código
+                conteo = (int)ViewState["click"] + 1;
+            }
+
+            // Actualizamos el textbox
+            txtConteo.Text = conteo.ToString();
+
+            // Asignamos el valor al viewsatate en la variable click
+            ViewState["click"] = conteo;
+        }
+    }
+}
---
+++ StateLess001/StateLess001/WebForm03.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateLess001
+{
+    public partial class WebForm03 : System.Web.UI.Page
+    {
+        //Los controles ASP.NET usan internamente a viewstate para guardar
+        // su estado
+        // Pero esto no nos quita de las responsabilidad de guardar los datos
+        // correctamente en el viewstate
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtConteo.Text = "0";
+            }
+        }
+
+        protected void btnIncrementa_Click(object sender, EventArgs e)
+        {
+            int conteo = Convert.ToInt32(txtConteo.Text) + 1;
+            txtConteo.Text = conteo.ToString();
+        }
+    }
+}
